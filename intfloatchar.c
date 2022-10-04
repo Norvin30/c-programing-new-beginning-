f28e317e9e394c8277d027f76ab5c22a73db4100
Norvin30 <108176736+Norvin30@users.noreply.github.com>
2022-10-04 20:01:10 +0530

    Add files via upload

---
+++ intfloatchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int myNum = 12;
+    float myFloatNum = 1.044;
+    char myLetter = 'a';
+
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
+    return 0;
+}
---
+++ sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 12;
+    int b = 12;
+    int sum = a + b;
+
+    printf("%d", sum);
+    return 0;
+}
